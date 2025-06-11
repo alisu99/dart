32111797d9e1701df181e513cc8bb354f1fd1ed9
Alisson Alencar <alissonsts910@gmail.com>
2025-06-11 10:36:17 -0300

    ...

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,18 +1,34 @@
 import 'dart:io';
 
+// void main() {
+//   print('Olá, digite seu nome abaixo:');
+//   var nome = stdin.readLineSync();
+//   print('Olá, $nome, Tudo bom? [S] sim [N] Não');
+
+//   var tudo_bom = stdin.readLineSync();
+
+//   if (tudo_bom == 'S') {
+//     print('Oba! que bom que está tudo bem com você!');
+//   } else if (tudo_bom == 'N') {
+//     print('Que pena!');
+//   } else {
+//     print('Não entendi o que você quis dizer!');
+//   }
+// }
+
+
 void main() {
-  print('Olá, digite seu nome abaixo:');
-  var nome = stdin.readLineSync();
-  print('Olá, $nome, Tudo bom? [S] sim [N] Não');
+  List<String> itens = ['Carro', 'Kotlin', 'Pêra', 'Louça', 'Pijama'];
+  // for (var i in itens) {
+  //   print(i);
+  // }
 
-  var tudo_bom = stdin.readLineSync();
+  print('Verificar item na lista, digite um iem:');
+  String item = stdin.readLineSync()!;
 
-  if (tudo_bom == 'S') {
-    print('Oba! que bom que está tudo bem com você!');
-  } else if (tudo_bom == 'N') {
-    print('Que pena!');
+  if (itens.contains(item)) {
+    print('O item $item está presente na lista.');
   } else {
-    print('Não entendi o que você quis dizer!');
+    print('O item $item não está na lista.');
   }
 }
-
