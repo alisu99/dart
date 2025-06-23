8f65d0d5243c0dc9683f3ede0da7f544e936cbed
Alisson Alencar <alissonsts910@gmail.com>
2025-06-23 19:38:08 -0300

    1

--- oo/bin/oo.dart
+++ oo/bin/oo.dart
@@ -1,14 +1,26 @@
 import '../lib/conta.dart';
 
-Conta contamateus = Conta("Mateus", 2000);
-Conta contaroberta = Conta("Roberta", 5000);
+// Conta contamateus = Conta("Mateus", 2000);
+// Conta contaroberta = Conta("Roberta", 5000);
 
+// void main() {
+//   contamateus.receber(200);
+//   contamateus.enviar(350);
+//   List<Conta> contas = [contamateus, contaroberta];
+
+//   for (var conta in contas) {
+//     conta.printSaldo();
+//   }
+// }
+
+
+Cliente clienteAlisson = Cliente("Alisson", [9, 12, 6, 7]);
+Cliente clienteAlice = Cliente("Alice", [8, 5, 11, 10]);
 void main() {
-  contamateus.receber(200);
-  contamateus.enviar(350);
-  List<Conta> contas = [contamateus, contaroberta];
+  // clienteAlisson.reservarQuarto(9);
+  // clienteAlisson.quartosReservados();
+  // clienteAlice.quartosReservados();
 
-  for (var conta in contas) {
-    print("${conta.titular} - ${conta._saldo}");
-  }
+  // clienteAlice.cancelarReserva(9);
+  clienteAlice.reservarQuarto(8);
 }
